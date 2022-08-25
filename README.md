b8598f74a368a8912eed932571f13689510f22b7
Travis-Findley <92493763+Travis-Findley@users.noreply.github.com>
2022-08-25 08:17:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python
+ This where my python scripts go
---
+++ StoryToBeDisided/newmovement.py
@@ -0,0 +1,46 @@
+import random as r
+
+
+
+def makingboard():
+    height, width = 10, 10
+    x = 0
+    y = 0
+    board = []
+    while (x <= height):
+        board += []
+        while (y <= width):
+            rand = r.randint(1,2)
+            if x == 0 or x == 10:
+                board[x, y] == 3
+            elif (y == 0 or y == 10):
+                if (x == 1):
+                    board[x, y] = rand
+                    board[x+1, y] = rand
+                elif (x == 9):
+                    board[x, y] = rand
+                    board[x-1, y] = rand
+                else:
+                    board[x, y] = 1
+            else:
+                board[x, y] = 0
+            y += 1
+        x += 1
+
+def printboard(board):
+    word = ""
+    for x in board:
+        for y in board[x]:
+            if board[x, y] == 0:
+                word += "  "
+            elif board[x, y] == 1:
+                word += "|"
+            elif board[x, y] == 3:
+                word += "~~"
+            elif board[x, y] == 2:
+                word += "#"
+        word += "\n"
+    print(word)
+
+board = makingboard()
+printboard(board)
