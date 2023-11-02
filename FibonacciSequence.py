5c2e444bbe44be9d5621047096e613027d0e1492
Travis Findley <92493763+Travis-Findley@users.noreply.github.com>
2023-11-02 15:39:51 -0500

    Added print all or index and made into one function

--- FibonacciSequence.py
+++ FibonacciSequence.py
@@ -1,36 +1,25 @@
-
-class sequence:
-
-    def __init__(self, startnum="no"):
-        self.sequence = [0, 1]
-        if startnum != "no":
-            self.intamount = startnum
-        else:
-            self.intamount = self.findhowmany(startnum)
-        self.actualyrun()
-        
-    def runnext(self):
-        self.sequence.append(self.sequence[-2] + self.sequence[-1])
-    
-    def findhowmany(self):
+def fibonacci(startnum, amount):
+    sequence = [0, 1]
+    if startnum != "no":
+        intamount = startnum
+        if intamount < 0:
+            return -1
+    else:
         user_input = input("How many numbers do you want?\n")
         while True:
             try:
                 return int(user_input)
             except:
                 user_input = input("Numbers only please\n")
+    for int in range(intamount):
+        sequence.append(sequence[-2] + sequence[-1])
+    while true:
+        amount = input(f"Do you want to see the whole sequence or just the one at the index {intamount} (the number you provided earlier)? [all, index]\n)")
     
-    def actualyrun(self):
-        for int in range(self.intamount):
-            self.runnext()
-    
-    def stringit(self):
-        ssequence = self.sequence
-        ssequence.remove(self.sequence[0])
-        printsequence = f"{self.sequence[0]}"
-        for int in ssequence:
-            printsequence += f", {int}"
-        return printsequence  
+        if amount == "all":
+            return print_sequence = ', '.join(map(str, sequence))
+        elif amount == "index":
+            return sequence[startnum]
 
 
 sf = sequence()
